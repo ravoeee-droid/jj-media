93d3e372aea10121adb21fa06e328eb152c1f5d0
ravoeee-droid <ravoeee@gmail.com>
2026-08-04 13:49:10 +0200

    chore: trigger production deployment for funnel release

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
   };
 
   if (document.querySelector('.hero-premium')) {
-    load('home-proof.js?v=20260804-1', () => load('app-core.js?v=20260804-1'));
+    load('home-proof.js?v=20260804-2', () => load('app-core.js?v=20260804-2'));
   } else {
-    load('app-core.js?v=20260804-1');
+    load('app-core.js?v=20260804-2');
   }
 })();
