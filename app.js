2fab548d5510b1ec91e2864b4186e330b66d8b34
ravoeee-droid <ravoeee@gmail.com>
2026-08-31 10:54:08 +0200

    fix: bust viral gallery cache after redesign

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
   };
 
   if (document.querySelector('.viral-page')) {
-    load('instagram-embeds.js?v=20260831-1');
+    load('instagram-embeds.js?v=20260831-4');
   }
 
   if (document.querySelector('.hero-premium')) {
