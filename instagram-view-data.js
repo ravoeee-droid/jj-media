823f8a8826a84f44b64aa2f793fcff854ad8b399
ravoeee-droid <ravoeee@gmail.com>
2026-08-31 11:29:41 +0200

    content: add verified reel view counts

--- instagram-view-data.js
+++ instagram-view-data.js
@@ -1,8 +1,8 @@
 window.JJ_MEDIA_REEL_VIEWS = {
-  DbNajc2Rdo4: null,
-  Dbf1jZOMaQt: null,
-  DcF9GhGxls2: null,
-  DbsVHalgz3N: null,
-  DZ44gXtvAob: null,
-  DareM3sxe6n: null
+  DbNajc2Rdo4: '17,4 Tsd.',
+  Dbf1jZOMaQt: '12,2 Tsd.',
+  DcF9GhGxls2: '60,7 Tsd.',
+  DbsVHalgz3N: '55 Tsd.',
+  DZ44gXtvAob: '21,4 Tsd.',
+  DareM3sxe6n: '20,2 Tsd.'
 };
